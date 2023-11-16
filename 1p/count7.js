1fb0eabd9858eccdd4ed14682c9a98c0b031f5dc
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-16 07:49:37 -0600

    Update count7.js
    Optimización del código y se anexan notas descriptivas.

--- 1p/count7.js
+++ 1p/count7.js
@@ -1,9 +1,9 @@
-// imprime el número de múltiplos de 7 en el rango [200-300] (¿cuantos?)
-let a, b, i;
-a=0;
-for(i=200;i<=300;i++){
-  if(i%7==0){
-    a= a + 1;
-  }
-}
-console.log("La cantidad de multiplos es igual a: "+ a);
+// Imprime el número de múltiplos de 7 en el rango [200-300] (¿cuantos?)
+
+// Se inicializan las variables para contar los múltiplos de 7 y establecer los límites del rango
+let a = Math.floor(200 / 7); // Determina cuántos múltiplos de 7 hay hasta el número 200
+let b = Math.floor(300 / 7); // Determina cuántos múltiplos de 7 hay hasta el número 300
+let result = b - a; // Calcula la diferencia para obtener los múltiplos entre 200 y 300
+
+// Imprime la cantidad de múltiplos de 7 en el rango [200-300]
+console.log("La cantidad de múltiplos es igual a: " + result);
