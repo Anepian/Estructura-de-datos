671236f2d5fa0245c0184652bf88c3ae681c28e7
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-16 07:53:23 -0600

    Update fib.js
    Código optimizado y se agregaron notas descriptivas.

--- 1p/fib.js
+++ 1p/fib.js
@@ -1,11 +1,16 @@
-let a,b=0, c=1, d;
-function fibonacci(a){
-  for(let i=1;i<=a;i++){
-    console.log(b);
-    d=b+c;
-    b=c;
-    c=d;
+// Descripción: Este programa calcula la secuencia de Fibonacci hasta un número dado.
+let previous = 0;
+let current = 1;
+let next;
+
+function fibonacci(limit) {
+  for (let i = 1; i <= limit; i++) {
+    console.log(current); // Imprime el número actual de la secuencia de Fibonacci
+    next = previous + current; // Calcula el siguiente número de la secuencia
+    previous = current; // Actualiza el valor anterior con el valor actual
+    current = next; // Actualiza el valor actual con el siguiente valor calculado
   }
 }
-a=11;
-fibonacci(a);
+
+const limit = 11; // Número hasta el cual se calculará la secuencia de Fibonacci
+fibonacci(limit); // Llama a la función para imprimir la secuencia hasta 'limit'
