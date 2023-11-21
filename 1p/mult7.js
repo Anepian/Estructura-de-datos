bb8c1c9b5f8d09a45409df213ef145629c89fc63
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:36:03 -0600

    Update mult7.js

--- 1p/mult7.js
+++ 1p/mult7.js
@@ -1,6 +1,9 @@
-// Multiplicar los multiplos de 7 del 200 a 300
+// Programa: Multiplicar los múltiplos de 7 en el rango del 200 al 300
+// Este programa busca los múltiplos de 7 en el rango del 200 al 299 y calcula el producto de todos ellos.
 
 var total;
+
+// Función que verifica si un número es múltiplo de otro
 function isMultiple(valor, mult) {
   var resp = valor % mult;
   if (resp == 0) {
@@ -10,13 +13,21 @@ function isMultiple(valor, mult) {
   }
 }
 
+// Función para encontrar los múltiplos y calcular su producto
 function multiple() {
   var min = 300;
   for (var i = 200; i < min; i++) {
     if (isMultiple(i, 7)) {
-      total = i * i;
+      // Multiplica los múltiplos de 7 encontrados
+      if (typeof total === 'undefined') {
+        total = i;
+      } else {
+        total *= i;
+      }
     }
   }
-  console.log("La multiplicatoria de los multiplos es igual a: " + total);
+  console.log("La multiplicatoria de los múltiplos es igual a: " + total);
+  return total;
 }
+
 console.log(multiple());
