925f23ac1718844960a200cf79fa4a78b9e8ed0f
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:43:06 -0600

    Update sucesion7.js

--- 1p/sucesion7.js
+++ 1p/sucesion7.js
@@ -1,8 +1,10 @@
-// Encontrar los multiplos de 7 del 200 al 300
+// Programa: Encontrar los múltiplos de 7 en el rango del 200 al 300
+// Este programa encuentra y muestra los números que son múltiplos de 7 en el rango del 200 al 300.
+
 let i;
 
-for(i=200;i<=300;i++){
-  if(i%7==0){
+for (i = 200; i <= 300; i++) {
+  if (i % 7 === 0) {
     console.log(i);
   }
 }
