bd69d285929cca7f0e59467c98de00f95eec9930
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:44:35 -0600

    Update suma7.js

--- 1p/suma7.js
+++ 1p/suma7.js
@@ -1,11 +1,13 @@
- // Sumar los multiplos de 7 del 200 a 300
-
+// Programa: Sumar los múltiplos de 7 en el rango del 200 al 300
+// Este programa suma todos los números que son múltiplos de 7 en el rango del 200 al 300.
 
 let a, b, i;
-a=0;
-for(i=200;i<=300;i++){
-  if(i%7==0){
-    a= i + a;
+a = 0;
+
+for (i = 200; i <= 300; i++) {
+  if (i % 7 === 0) {
+    a = i + a;
   }
 }
-console.log("La suma de los multiplos es igual a: "+ a);
+
+console.log("La suma de los múltiplos es igual a: " + a);
