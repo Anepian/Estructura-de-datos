f5118af4ec5c0e7d37257124c6d4c2474a7da213
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:45:51 -0600

    Update triangulares.js

--- 1p/triangulares.js
+++ 1p/triangulares.js
@@ -1,10 +1,15 @@
-let x,y;
+// Programa: Números triangulares hasta un límite dado
+// Este programa calcula y muestra los números triangulares hasta el límite dado 'x'.
 
-function triangulares(x){
-  for(let i=1;i<=x;i++){
-    y=i*(i+1)/2;
+let x, y;
+
+// Función para generar números triangulares hasta el límite dado
+function triangulares(x) {
+  for (let i = 1; i <= x; i++) {
+    y = i * (i + 1) / 2;
     console.log(y);
   }
 }
-x=21;
+
+x = 21; // Límite hasta el cual se generarán números triangulares
 triangulares(x);
