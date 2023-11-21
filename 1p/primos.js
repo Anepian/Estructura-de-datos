835274583d417949530472e2b47bcb4e63f4897d
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:39:41 -0600

    Update primos.js

--- 1p/primos.js
+++ 1p/primos.js
@@ -1,23 +1,28 @@
-let a, b, c, d, aux;
-aux=0;
+// Programa: Generar los primeros 'a' números primos
+// Este programa genera los primeros 'a' números primos e imprime cada número primo encontrado.
 
-function primos(a)  {
-  b=0;
-  while(a > 0){
-    b = b+1;
+let a, b, c, d, aux;
+aux = 0;
+
+// Función para encontrar los primeros 'a' números primos
+function primos(a) {
+  b = 0;
+  while (a > 0) {
+    b = b + 1;
     c = 0;
-    d= 0;
-    while(c <= b) {
-      if(b % c == 0){
+    d = 0;
+    while (c <= b) {
+      if (b % c == 0) {
         d++;
       }
-      c++
+      c++;
     }
-    if (d == 2){
+    if (d == 2) {
       console.log(b);
       a--;
     }
   }
 }
-a = 11;
+
+a = 11; // Número de números primos que se desean encontrar
 primos(a);
