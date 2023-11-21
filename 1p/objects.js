608dc62ba0346f0124cbff5f187e5345e46b3b0c
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:37:33 -0600

    Update objects.js

--- 1p/objects.js
+++ 1p/objects.js
@@ -1,8 +1,11 @@
+// Programa: Generar un objeto con nombres aleatorios y contar su cantidad
+// Este programa crea un objeto con 10,000 nombres aleatorios y luego cuenta cuántos nombres únicos hay en el objeto.
+
 let obj = {};
 
 for (let i = 1; i <= 1e4; i++) {
   let name = 'Name:' + Math.floor(Math.random() * 1e4);
   obj[name] = name;
 }
-console.log(obj)
-console.log(Object.values(obj).length)
+console.log(obj);
+console.log(Object.values(obj).length);
