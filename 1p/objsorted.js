91b788ebd0a8672e6f6848be15c9366e8712425a
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:38:59 -0600

    Update objsorted.js

--- 1p/objsorted.js
+++ 1p/objsorted.js
@@ -1,14 +1,17 @@
+// Programa: Generar un objeto con números aleatorios y ordenar sus claves en orden descendente
+// Este programa crea un objeto con 10,000 números aleatorios como claves y los ordena en orden descendente en un nuevo objeto.
+
 let a = {}, b = {};
 
-for (i = 1; i <= 10e3; i++) {
-  n = Math.floor(Math.random() * 10e3);
+for (let i = 1; i <= 10e3; i++) {
+  let n = Math.floor(Math.random() * 10e3);
   a[n] = n;
 }
-c = Object.keys(a)
-c.sort((x, z) => x < z ? 1 : z > x ? -1 : 0)
-for (i = 0; i < c.length; i++) {
-  let name = "name:"
-  b[name + c[i]] = name + c[i]
+let c = Object.keys(a);
+c.sort((x, z) => x < z ? 1 : z > x ? -1 : 0);
+for (let i = 0; i < c.length; i++) {
+  let name = "name:";
+  b[name + c[i]] = name + c[i];
 }
 
-console.log(b)
+console.log(b);
