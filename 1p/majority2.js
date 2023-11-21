cc2bed0ea12f6fd4f950278631f0ace703b9374f
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-21 10:34:27 -0600

    Update majority2.js

--- 1p/majority2.js
+++ 1p/majority2.js
@@ -1,43 +1,51 @@
-let a = [], b = [], x, y, nr, k = 0, repetido = 0;
+// Programa: Encontrar el número mayoritario en un array generado aleatoriamente
+// Este programa genera un array de números aleatorios y busca el número que se repite más de la mitad de las veces en el array.
+
+let a = [],
+  b = [],
+  x,
+  y,
+  nr,
+  k = 0,
+  repetido = 0;
+
 function aleatorio(a) {
   a.length = x;
   b.length = y;
-  for (j = 0; j <= x - 1; j++) {
+  for (let j = 0; j <= x - 1; j++) {
     a[j] = Math.floor(Math.random() * ((x - 1) - 1 + 1) + 1);
-    //generar los 5 grupos de numeros aleatorios
+    //generar los 5 grupos de números aleatorios
   }
-  console.log(a);//mostrar array desordenado
+  console.log(a); //mostrar array desordenado
 }
 
 function repeticiones(x) {
-  y = x - 1
-  aleatorio(a)
-  for (l = 0; l <= y; l++) {
+  y = x - 1;
+  aleatorio(a);
+  for (let l = 0; l <= y; l++) {
     b[l] = 0;
   }
   k = 1;
   do {
-    for (i = 0; i < a.length; i++) {
-      //alert(k+ "=="+ a[i] + "?")
+    for (let i = 0; i < a.length; i++) {
       if (k == a[i]) {
-        b[k - 1] = parseInt(b[k - 1]) + 1
+        b[k - 1] = parseInt(b[k - 1]) + 1;
       }
     }
     k++;
   } while (k < x);
-  for (j = 0; j < b.length; j++) {
+  for (let j = 0; j < b.length; j++) {
     if (b[j] >= repetido) {
       repetido = b[j];
       nr = j + 1;
     }
   }
-  aux = Math.floor((a.length / 2))
+  const aux = Math.floor(a.length / 2);
   if (repetido > aux) {
-    console.log("El numero mayoritario es: " + nr + " repitiendose: " + repetido + " veces")
-  }
-  else {
-    console.log("No existe un numero mayoritario")
+    console.log("El número mayoritario es: " + nr + " repitiéndose: " + repetido + " veces");
+  } else {
+    console.log("No existe un número mayoritario");
   }
 }
 
-repeticiones(x = 5)
+repeticiones(x = 5);
