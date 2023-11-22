cceb02779b1597b4326bade92de28bdb38e13f49
Alejandro Vizcaino Lupian <99756995+Anepian@users.noreply.github.com>
2023-11-22 08:56:22 -0600

    Update stack-loose.js

--- 2p/3stack/0bj/stack-loose.js
+++ 2p/3stack/0bj/stack-loose.js
@@ -1,31 +1,25 @@
-//
-// loose code
-// vs
-// asClass code
-// 
-
 console.log('Hello stack DS world in ES6+\n')
 
-let a = []
+let a = [] // Se crea un array vacío llamado 'a'
 
-a.push('a')
-console.log(a)
+a.push('a') // Se agrega el elemento 'a' al array 'a'
+console.log(a) // Se imprime el array: ['a']
 
-a.push('b')
-console.log(a)
+a.push('b') // Se agrega el elemento 'b' al array 'a'
+console.log(a) // Se imprime el array: ['a', 'b']
 
-a.push('d')
-console.log(a)
+a.push('d') // Se agrega el elemento 'd' al array 'a'
+console.log(a) // Se imprime el array: ['a', 'b', 'd']
 
-a.forEach(k => console.log(k))
+a.forEach(k => console.log(k)) // Se realiza un ciclo forEach para imprimir cada elemento del array en líneas separadas
 
-a.pop()
-console.log(a)
+a.pop() // Se remueve el último elemento del array 'a' ('d')
+console.log(a) // Se imprime el array después de la operación de pop: ['a', 'b']
 
-a.pop()
-console.log(a)
-console.log(a.length)
+a.pop() // Se remueve el último elemento del array 'a' ('b')
+console.log(a) // Se imprime el array después de la operación de pop: ['a']
+console.log(a.length) // Se imprime la longitud actual del array 'a' (1)
 
-a.pop()
-console.log(a)
-console.log(a.length)
+a.pop() // Se remueve el último elemento del array 'a' ('a')
+console.log(a) // Se imprime el array después de la operación de pop: []
+console.log(a.length) // Se imprime la longitud actual del array 'a' (0)
